8eb44815a054a102908365a4df736f76f8b1573e
T.Y. Lee <tyl35@pcphy108.phy.pwf.cam.ac.uk>
2018-02-12 16:49:02 +0000

    added probability to ising.py

--- ising.py
+++ ising.py
@@ -7,15 +7,15 @@ N x N lattice
 import numpy as np
 
 def DelEnergy(M,H,mu,J,i,j):
-	sisj=M[i,j]*(M[i+1,j]+M[i-1,j]+M[i,j-1]+M[i,j+1])
-	return(2*J*sisj+mu*H*M[i,j])
-def flip(M,N):
-	sisj=M[i,j]*(M[i+1,j]+M[i-1,j]+M[i,j-1]+M[i,j+1])
-	if 2*J*sisj+mu*H*M[i,j]<0:
-
+	s=M[i,j]
+	coup=2*s*(M[i+1,j]+M[i-1,j]+M[i,j-1]+M[i,j+1]) #spin coupling term
+	ext=2*mu*H*s #field energy
+	print(ext+coup)
+	return(np.random.rand(1)<np.exp(-1*max([ext+coup,0])/(kT)))		
 N=100
 M=np.random.choice([-1,1],[N,N])
 print(M)
 
-H,mu,J=1,1,1
+H,mu,J,kT=1,1,1,1
 print(DelEnergy(M, H, mu, J, 1,1))
+
