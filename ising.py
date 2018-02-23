461ae8507220369233d0a90dcb33eb60d1e183cb
Tim Lee <timlee@tyl35c.pem.private.cam.ac.uk>
2018-02-23 00:03:17 +0000

    upadted ising.py to include range of N

--- ising.py
+++ ising.py
@@ -1,35 +1,36 @@
 """
-Project C: The Ising Model of a Ferromagnet
-                                                                                                                                                                                                                                                                                                                                                                                                                       ukjjhC: The Ising Model of a Ferromagnet
-Finds properties of a Ferromagnet using the Ising model, sampling spins at random and calculating the energy required to flip each spin
+Project C: The Ising Model of a Ferromagnet                                                                                                                                                                                                                                                                                                                                                                                                                       ukjjhC: The Ising Model of a Ferromagnet
+-------------------------------------------
+Finds properties of a Ferromagnet using the Ising model, sampling spins at
+random and calculating the energy required to flip each spin
 
 N x N lattice
 """
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as op
-import pdb
 
 
 def movingAvg(arr, n):
     csum = arr.cumsum()
-    csum[n:] = csum[n:]-csum[:-n]
-    return(csum[n-1:]/n)
+    csum[n:] = csum[n:] - csum[:-n]
+    return(csum[n - 1:] / n)
 
 
 def DelEnergy(M, H, mu, J, i, j, kT):
     s = M[i, j]
-    coup = 2*s*(M[i+1, j]+M[i-1, j]+M[i, j-1]+M[i, j+1])  # spin coupling term
-    ext = 2*mu*H*s  # field energy
+    coup = 2 * s * (M[i + 1, j] + M[i - 1, j] + M[i, j - 1] +
+                    M[i, j + 1])  # spin coupling term
+    ext = 2 * mu * H * s  # field energy
     # print(ext+coup)
-    return((ext+coup < 0) or (np.random.rand(1) < np.exp(-1*(ext+coup)/(kT))))
+    return((ext + coup < 0) or (np.random.rand(1) < np.exp(-1 * (ext + coup) / (kT))))
 
 
 def MCstep(N, M):
     """performs each step of the MC technique, each sampling N^2=Ntot points in the lattice"""
     """on average covers ~63 lattice sites (see readme for calculation)"""
     samples = np.random.permutation(
-        np.array([[i, j] for i in range(N) for j in range(N)])-1)
+        np.array([[i, j] for i in range(N) for j in range(N)]) - 1)
     for sample in samples:
         if DelEnergy(M, H, mu, J, sample[0], sample[1], kT):
             M[sample[0], sample[1]] *= -1
@@ -37,8 +38,8 @@ def MCstep(N, M):
 
 
 def meanEnergy(Mag, H, mu, J):
-    eng = -1.*Mag*mu*H-J/2*Mag*(np.roll(Mag, 1, axis=1)+np.roll(
-        Mag, -1, axis=1)+np.roll(Mag, 1, axis=2)+np.roll(Mag, -1, axis=2))
+    eng = -1. * Mag * mu * H - J / 2 * Mag * (np.roll(Mag, 1, axis=1) + np.roll(
+        Mag, -1, axis=1) + np.roll(Mag, 1, axis=2) + np.roll(Mag, -1, axis=2))
     return(eng.sum(axis=(1, 2)).mean())
 
 
@@ -47,21 +48,22 @@ def makeM(N, p):
         return(np.ones((N, N)))
     else:
         M = np.concatenate(
-            (np.ones(int(N**2*p)), -1*np.ones(N**2-int(N**2*p))))
+            (np.ones(int(N**2 * p)), -1 * np.ones(N**2 - int(N**2 * p))))
         M = (np.random.permutation(M)).reshape((N, N))
         return(M)
 
 
 def autoCorr(inMag, tauArr):
-    MMag = inMag-inMag.mean()
-    if type(tauArr) == int:
+    MMag = inMag - inMag.mean()
+    if isinstance(tauArr, int):
         tau = tauArr
-        return(np.mean(MMag[tau:]*MMag[:-tau])/np.mean(MMag**2))
+        return(np.mean(MMag[tau:] * MMag[:-tau]) / np.mean(MMag**2))
     else:
-        return(np.array([np.mean(MMag[tau:]*MMag[:-tau])/np.mean(MMag**2)for tau in tauArr]))
+        return(np.array([np.mean(MMag[tau:] * MMag[:-tau]) / np.mean(MMag**2)
+                         for tau in tauArr]))
 
 
-autoCorrV = np.vectorize(autoCorr)
+autoCorrV = np.vectorize(autoCorr, excluded=['inMag'])
 
 
 def findTauc(inMag, initTau):
@@ -69,32 +71,48 @@ def findTauc(inMag, initTau):
 
 
 N = 10
-Tsize = 10
+Narr = np.arange(8, 17, 2)
+Tsize = 15
 H, mu, J = 0, 1, 1
-kTarr = np.linspace(2, 4, Tsize)*J
-eArr = np.zeros(Tsize)
-MArr = np.zeros(Tsize)
-arrSize = 300
-tauC = np.zeros((5, Tsize))
-fig1, ax1 = plt.subplots()
+kTarr = np.linspace(1.5, 3.5, Tsize) * J
+nSamp = 6
+eArr = np.zeros((nSamp, Tsize))
+MArr = np.zeros((nSamp, Tsize))
+arrSize = 500
+tauC = np.zeros((nSamp, Tsize))
 plotTau = np.arange(1, 10)
-
-for j in range(Tsize):
-    kT = kTarr[j]
-    for i in range(5):
-        Mag = np.zeros((arrSize, N, N))
-        Mag[0] = (makeM(N, 1))
-        for k in range(1, arrSize):
-            Mag[k] = MCstep(N, Mag[i-1])
-        inMag = Mag[50:].sum(axis=(1, 2))
-        #ax2.plot(plotTau, np.log(SArr.mean))
-        for tau in range(1, 41):
-            if autoCorr(inMag, tau) < np.exp(-1):
-                tauC[i, j] = tau-0.5
-                break
+fig1, ax1 = plt.subplots()
+fig3, ax3 = plt.subplots()
 fig2, ax2 = plt.subplots()
-ax2.errorbar(kTarr, tauC.mean(axis=0), tauC.std(axis=0))
 
+for N in Narr:
+    for j in range(Tsize):
+        kT = kTarr[j]
+        for i in range(nSamp):
+            Mag = np.zeros((arrSize, N, N))
+            Mag[0] = (makeM(N, 1))
+            for k in range(1, arrSize):
+                Mag[k] = MCstep(N, Mag[k - 1])
+            inMag = Mag[50:].sum(axis=(1, 2))
+            """
+            if np.abs(kT - 2.6) < 0.1:
+                ax3.plot(np.log(np.abs(
+                    autoCorr(inMag=inMag, tauArr=np.arange(1, 41)))))
+            """
+            eArr[i, j] = np.abs(meanEnergy(Mag, H, mu, J))
+            MArr[i, j] = np.abs(inMag.mean())
+            for tau in range(1, 41):
+                if np.abs(autoCorr(inMag, tau)) < np.exp(-1):
+                    tauC[i, j] = tau - 0.5
+                    break
+    ax2.errorbar(kTarr, tauC.mean(axis=0), tauC.std(axis=0), label=r'$N=%i$'%N)
+    ax1.errorbar(kTarr, eArr.mean(axis=0), eArr.std(axis=0), label=r'$N=%i$'%N)
+    ax3.errorbar(kTarr, MArr.mean(axis=0), MArr.std(axis=0), label=r'$N=%i$'%N)
+
+ax1.legend()
+ax2.legend()
+ax3.legend()
+"""
 for j in range(Tsize):
     kT = kTarr[j]
     M = makeM(N, 1)
@@ -102,19 +120,17 @@ for j in range(Tsize):
     for i in range(arrSize):
         Mag[i] = M
         M = MCstep(N, M)
-    if j % int(Tsize/5) == 0:
+    if j % int(Tsize / 5) == 0:
         Mplot = movingAvg(Mag.sum(axis=(1, 2)), 10)
-        ax1.plot(np.sign(Mplot[0])*Mplot)
+        ax1.plot(np.sign(Mplot[0]) * Mplot)
     eArr[j] = meanEnergy(Mag, H, mu, J)
     MArr[j] = np.abs(Mag.sum(axis=(1, 2)).mean())
-    #print(kT, autoCorr(Mag[50:].sum(axis=(1,2)),(arrSize-51))-np.exp(-1.))
-
-
+    # print(kT, autoCorr(Mag[50:].sum(axis=(1,2)),(arrSize-51))-np.exp(-1.))
+"""
 fig, ax = plt.subplots()
-ax.plot(kTarr, eArr, label='mean energy')
+ax.errorbar(kTarr, eArr.mean(axis=0), eArr.std(axis=0), label='mean energy')
 fig, ax = plt.subplots()
-ax.plot(kTarr, MArr, label='mean magnetisation')
+ax.errorbar(kTarr, MArr.mean(axis=0), MArr.std(axis=0), label='mean magnetisation')
 # fig,ax=plt.subplots()
-#ax.plot(kTarr,tauC,label='mean magnetisation')
+# ax.plot(kTarr,tauC,label='mean magnetisation')
 plt.show()
-
--- readme.md
+++ readme.md
@@ -1,9 +1,23 @@
-# Computing Project C: Ising Model of Ferromagnets
+---
+title: "**Ising Model of Ferromagnets**"
+author: Tim T. Y. Lee
+geometry: margin=2cm
+output: pdf_document
+numbersections: true
+abstract: "This project is an investigation into the use of Monte-Carlo methods to tackle the Ising model of ferromagnetism which considers the interaction between spins of nearest neighbours. For simplicity, the 2-dimensional case is chosen meaning we only consider energy contributions from the four nearest neighbours of each spin. The model gives a way to evaluate the total energy of each microstate from which we can derive the probability of a certain change occurring(specifically the probability of a spin flipping). Hence, by generating random samples of the system at certain time steps, we can model the time evolution of the system given the temperature $T$."
+---
 
-This project is an investigation into the use of Monte-Carlo methods to tackle the Ising model of ferromagnetism which considers the interaction between spins of nearest neighbours. For simplicity, the 2-dimensional case is chosen meaning we only consider energy contributions from the four nearest neighbours of each spin. The model gives a way to evaluate the total energy of each microstate from which we can derive the probability of a certain change occurring(specifically the probability of a spin flipping). Hence, by generating random samples of the system at certain time steps, we can model the time evolution of the system given the temperature $T$.
+# Introduction
 
-## Background Theory
+Ferromagnets are materials in which spin orientations are able to exhibit long range order at low enough temperatures, the occurrence of such behaviour is a well known case of spontaneous symmetry breaking exemplifying how a system chooses a preferred direction. The Ising model is an attempt at describing the behaviour of spins within a ferromagnet by considering the spin interaction of closest neighbours only, while this may be a substantial simplification, it can be a powerful starting point as it lets us write down a relatively simple closed form Hamiltonian.
+
+A Monte-Carlo method is best understood as a random sampling of a function's parameter space, which when done sufficiently gives a reasonable estimate for the solution. The rate at which the results converges will depend on the size of the parameter space and how well behaved the function is, in particular, if there are divergence points within the parameter space, results may vary dramatically if not treated properly. Over the course of this investigation, the effects of said divergences will be analysed and discussed in the context of the Ising Model, for which a discontinuity occurs at some critical temperature $T_c$.
+
+Our aim is to find reasonable solutions to the Ising model in 2 dimensions via a Monte-Carlo method from which we can extract information about the properties of a ferromagnet. 
+
+# Background Theory
 
 The energy of a set of spins in a regular lattice in the presence of some magnetic field can be given by:
 
-$$ E=-J\sum_{<ij>} s_i s_j + H\mu \sum_{i=1}^{N^2}$$
+\begin{equation}  E=-J\sum_{<ij>} s_i s_j - H\mu \sum_{i=1}^{N^2} \label{eq:Hamil} \end{equation}
+
