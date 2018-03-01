119625ba238dc95f4e13a44f17b34589ac51fa88
Tim Lee <timlee@dhcp-10-248-103-200.eduroam.wireless.private.cam.ac.uk>
2018-03-01 12:01:57 +0000

    addded curve fitting for finite-size scaling function

--- ising.py
+++ ising.py
@@ -96,21 +96,24 @@ def findTauc(inMag, initTau):
     return(op.brentq((lambda tau: autoCorr(inMag, int(tau)) - np.exp(-1.)), 1, int(initTau)))
 
 
+def fitTc(Tc, x, NArr):
+    [Tc_inf, a, v] = x
+    return(((Tc - Tc_inf - a * NArr**(-1/v))**2).sum())
+
 autoCorrV = np.vectorize(autoCorr, excluded=['inMag'])
 
-NArr = np.arange(10, 21, 2)                 # Array that holds the values of N to be used
-NSize = len(NArr)
+NArr = np.arange(10, 51, 4)             # Array that holds the values of N to be used
 nRelax = 50
+nSamp = 3                               # Number of samples to run
 TSize = 30                              # Number of samples of temperature to be used
 H, mu, J = 0, 1, 1
-kTarr = np.linspace(1, 4, TSize) * J  # kT scaled relative to J
-nSamp = 4                               # Number of samples to run
-arrSize = 150                           # Number of steps to take for each M-C simulation
-eArr = np.zeros((nSamp, TSize, 2))
+kTarr = np.linspace(1, 4, TSize) * J    # kT scaled relative to J
+arrSize = 250                           # Number of steps to take for each M-C simulation
+eArr = np.zeros((nSamp, TSize))
 MArr = np.zeros((nSamp, TSize))
-CMaxArr = np.zeros(NSize)
+CArr = np.zeros((nSamp, TSize))
+CMaxArr = np.zeros(nSamp)
 tauC = np.zeros((nSamp, TSize))
-# plotTau = np.arange(1, 10)
 
 fig1, ax1 = plt.subplots()
 fig2, ax2 = plt.subplots()
@@ -118,35 +121,67 @@ fig3, ax3 = plt.subplots()
 fig4, ax4 = plt.subplots()
 fig5, ax5 = plt.subplots()
 
-for l in range(NSize):
+N = 10
+for j in range(TSize):
+    kT = kTarr[j]
+    print('N= %i, kT= %f' % (N, kT))
+    for i in range(nSamp):
+        #t0 = time.clock()
+        Mag = MCStepFast(H, mu, J, kT, arrSize)
+        #tf = time.clock()
+        inMag = Mag[nRelax:].sum(axis=(1, 2))
+        eArr[i, j] = (meanEnergy(Mag[nRelax:], H, mu, J))[0]
+        MArr[i, j] = np.abs(inMag.mean())
+        #tf2 = time.clock()
+        CArr[i, j] = (meanEnergy(Mag[nRelax:], H, mu, J)[1]**2)/(kT**2)
+        #print(tf - t0, tf2 - t0)
+for i in range(nSamp):
+        CMaxArr[i] = kTarr[np.argmax(CArr[i])]
+
+
+print('N= {:d}, T_c= {:.2f}±{:.2f}'.format(N, CMaxArr.mean(), CMaxArr.std()))
+ax3.errorbar(kTarr, MArr.mean(axis=0), MArr.std(axis=0), label=r'$N=%i$' % N)
+ax2.errorbar(kTarr, eArr.mean(axis=0), eArr.std(axis=0), label=r'$N=%i$' % N)
+
+NArr = np.arange(2, 15, 1)             # Array that holds the values of N to be use
+nSamp = 3
+eArr = np.zeros((nSamp, TSize))
+MArr = np.zeros((nSamp, TSize))
+CMaxArr = np.zeros((nSamp, len(NArr)))
+tauC = np.zeros((nSamp, TSize))
+
+for l in range(len(NArr)):
     N = NArr[l]
     for j in range(TSize):
         kT = kTarr[j]
+        print('N= %i, kT= %f' % (N, kT))
         for i in range(nSamp):
-            t0 = time.clock()
+            #t0 = time.clock()
             Mag = MCStepFast(H, mu, J, kT, arrSize)
-            tf = time.clock()
+            #tf = time.clock()
             inMag = Mag[nRelax:].sum(axis=(1, 2))
-            eArr[i, j] = (meanEnergy(Mag[nRelax:], H, mu, J))
-            MArr[i, j] = np.abs(inMag.mean())
-            for tau in range(1, arrSize-201):
+            eArr[i, j] = (meanEnergy(Mag[nRelax:], H, mu, J))[1]    # Standard deviation in total energy
+            for tau in range(1, arrSize-nRelax-1):
                 if np.abs(autoCorr(inMag, tau)) < np.exp(-1):
                     tauC[i, j] = tau - 0.5
                     break
-            tf2 = time.clock()
-            print(tf - t0, tf2 - t0)
-    C = (eArr[:, :, 1])**2/(kT**2)
-    if N % 1 == 0:
+            #tf2 = time.clock()
+            #print(tf - t0, tf2 - t0)
+    C = (eArr**2)/(kT**2)                                           # Heat capacity from
+    if N % 2 == 0:
         ax1.errorbar(kTarr, tauC.mean(axis=0), tauC.std(axis=0), label=r'$N=%i$' % N)
-        ax2.errorbar(kTarr, eArr[:, :, 0].mean(axis=0), eArr[:, :, 0].std(axis=0), label=r'$N=%i$' % N)
-        ax3.errorbar(kTarr, MArr.mean(axis=0), MArr.std(axis=0), label=r'$N=%i$' % N)
         ax4.errorbar(kTarr, C.mean(axis=0), C.std(axis=0), label=r'$N=%i$' % N)
-    CMaxArr[l] = kTarr[np.argmax(C.mean(axis=0))]
-
+    for i in range(nSamp):
+        CMaxArr[i, l] = kTarr[np.argmax(C[i])]
+x0 = [2, 1, 1]
+xOpt = op.minimize(lambda x: fitTc(CMaxArr.mean(axis=0), x, NArr), x0)
+x0 = xOpt.x
+print(x0)
 ax1.legend()
 ax2.legend()
 ax3.legend()
 ax4.legend()
 
-ax5.errorbar(NArr, CMaxArr, kTarr[1]-kTarr[0])
+ax5.plot(NArr, x0[0] + x0[1] * NArr**(-1/x0[2]))
+ax5.errorbar(NArr, CMaxArr.mean(axis=0), CMaxArr.std(axis=0), marker='+')
 plt.show()
