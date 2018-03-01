a63cf487c1cc9650902348d30e2156af599a8859
T.Y. Lee <tyl35@pcphy101.phy.pwf.cam.ac.uk>
2018-03-01 16:09:40 +0000

    to be fixed - multithreading seemes to break trend in CMax

--- ising.py
+++ ising.py
@@ -105,19 +105,23 @@ def fitTc(Tc, x, NArr):
 def mainRun(kT):
     NSize = len(NArr)
     sigEArr = np.zeros((nSamp, NSize))
+    MArr = np.zeros((nSamp, NSize))
     tauC = np.zeros((nSamp, NSize))
-    for [i, j] in [[i, j] for i in range(nSamp) for j in range(NSize)]:
-        N = NArr[j]
-        print('N= %i, kT= %f' % (N, kT))
-        Mag = MCStepFast(N, H, mu, J, kT, arrSize)
-        inMag = Mag[nRelax:].sum(axis=(1, 2))
-        sigEArr[i, j] = (meanEnergy(Mag[nRelax:], H, mu, J))[1]    # Standard deviation in total energy
-        for tau in range(1, arrSize-nRelax-1):
-            if np.abs(autoCorr(inMag, tau)) < np.exp(-1):
-                tauC[i, j] = tau - 0.5
-                break
+    for i in range(nSamp):
+        for j in range(NSize):
+            N = NArr[j]
+            print('N= %i, kT= %f' % (N, kT))
+            Mag = MCStepFast(N, H, mu, J, kT, arrSize)
+            inMag = Mag[nRelax:].sum(axis=(1, 2))
+            sigEArr[i, j] = (meanEnergy(Mag[nRelax:], H, mu, J))[1]    # Standard deviation in total energy
+            MArr[i, j] = np.abs(inMag.mean()) / N**2
+            for tau in range(1, arrSize-nRelax-1):
+                if np.abs(autoCorr(inMag, tau)) < np.exp(-1):
+                    tauC[i, j] = tau - 0.5
+                    break
     C = (sigEArr**2)/(kT**2)
-    return([C, tauC])
+    return([C, tauC, MArr])
+
 
 nRelax = 50
 nSamp = 4                               # Number of samples to run
@@ -136,23 +140,29 @@ eArr = np.zeros((nSamp, TSize))
 sigEArr = np.zeros((nSamp, TSize))
 MArr = np.zeros((nSamp, TSize))
 CMaxArr = np.zeros((nSamp, len(NArr)))
-tauC = np.zeros((nSamp, TSize))
 
 p = mp.Pool(len(kTArr))
-out = np.transpose(np.array(p.map(mainRun, kTArr)), axes=(1, 2, 3, 0))
+out = np.moveaxis(np.array(p.map(mainRun, kTArr)), 0, -1)
 print(out.shape)
-[CArr, tauCArr] = out
+[CArr, tauCArr, MArr] = out
 
 for l in range(len(NArr)):
+    N = NArr[l]
+    C = CArr[:, l]
+    M = MArr[:, l]
+    tauC = tauCArr[:, l]
     for i in range(nSamp):
         CMaxArr[i, l] = kTArr[np.argmax(CArr[i, l])]
-
+    if NArr[l] % 2 == 0:
+        ax1.errorbar(kTArr, tauC.mean(axis=0), tauC.std(axis=0), label=r'$N=%i$' % N)
+        ax4.errorbar(kTArr, C.mean(axis=0), C.std(axis=0), label=r'$N=%i$' % N)
+        ax3.errorbar(kTArr, M.mean(axis=0), M.std(axis=0), label=r'$N=%i$' % N)
 x0 = [2, 4, 0.5]
 xOpt = op.minimize(lambda x: fitTc(CMaxArr.mean(axis=0), x, NArr), x0)
 print(xOpt.x)
-x0= xOpt.x
 try:
-    xCF, xCov = op.curve_fit((lambda NArr, Tc_inf, a, v: Tc_inf + a * NArr**(-1/v)), NArr, CMaxArr.mean(axis=0), x0)
+    xCF, xCov = op.curve_fit((lambda NArr, Tc_inf, a, v: Tc_inf + a * NArr**(-1/v)), NArr, CMaxArr.mean(axis=0), x0,
+                             sigma=CMaxArr.std(axis=0))
     x0 = xCF
 except RuntimeError:
     x0 = xOpt.x
@@ -160,13 +170,12 @@ except RuntimeError:
 print(x0)
 ax5.plot(NArr, x0[0] + x0[1] * NArr**(-1/x0[2]))
 ax5.plot(NArr, 2/np.log(1 + np.sqrt(2))*np.ones(len(NArr)))
-ax5.errorbar(NArr, CMaxArr.mean(axis=0), CMaxArr.std(axis=0), marker='+')
+ax5.errorbar(NArr, CMaxArr.mean(axis=0), CMaxArr.std(axis=0))
 
+"""
 nSamp = 5
 MArr = np.zeros((nSamp, TSize))
-CArr = np.zeros((nSamp, TSize))
 eArr = np.zeros((nSamp, TSize))
-
 N = 10
 for j in range(TSize):
     kT = kTArr[j]
@@ -175,11 +184,10 @@ for j in range(TSize):
         Mag = MCStepFast(N, H, mu, J, kT, arrSize)
         inMag = Mag[nRelax:].sum(axis=(1, 2))
         eArr[i, j] = (meanEnergy(Mag[nRelax:], H, mu, J))[0]
-        MArr[i, j] = np.abs(inMag.mean())
+        MArr[i, j] = (inMag.mean())
 print('N= {:d}, T_c= {:.2f}±{:.2f}'.format(N, CMaxArr.mean(), CMaxArr.std()))
 ax3.errorbar(kTArr, MArr.mean(axis=0), MArr.std(axis=0), label=r'$N=%i$' % N)
 ax2.errorbar(kTArr, eArr.mean(axis=0), eArr.std(axis=0), label=r'$N=%i$' % N)
-"""
 for l in range(len(NArr)):
     N = NArr[l]
     for j in range(TSize):
